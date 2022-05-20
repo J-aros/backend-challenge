00a20844998d3725e086ad48fbaec7ca6efebe67
J-aros <j.aros.cortes@gmail.com>
2022-05-20 02:07:06 -0400

    feat: Add controller

---
+++ src/controllers/car.controller.ts
@@ -0,0 +1,70 @@
+import { Request, Response } from "express";
+import { cars } from "../models/cars";
+
+export const getCar = (req: Request, res: Response) => {
+  const car = cars.find((c) => c.id === parseInt(req.params.id));
+  if (!car) {
+    res.status(404).send("The car with the given ID was not found.");
+    return;
+  }
+  res.send(car);
+};
+
+export const getCars = (req: Request, res: Response) => {
+  res.json(cars);
+};
+
+export const createCar = (req: Request, res: Response) => {
+  const { car, car_model, car_model_year } = req.body;
+  if (!car || !car_model || !car_model_year) {
+    return res.send({
+      error: true,
+      message: "car, model, and year are required",
+      status: 400,
+    });
+  } else {
+    const newCar = {
+      id: cars.length + 1,
+      ...req.body,
+    };
+    cars.push(newCar);
+    res.json(newCar);
+  }
+};
+
+export const updateCar = (req: Request, res: Response) => {
+  const { car, car_model, car_model_year } = req.body;
+  if (!car || !car_model || !car_model_year) {
+    return res.send({
+      error: true,
+      message: "car, model, and year are required",
+      status: 400,
+    });
+  } else {
+    const carIndex = cars.findIndex((c) => c.id === parseInt(req.params.id));
+    if (carIndex === -1) {
+      return res.send({
+        error: true,
+        message: "The car with the given ID was not found.",
+        status: 404,
+      });
+    }
+    const updatedCar = {
+      id: parseInt(req.params.id),
+      ...req.body,
+    };
+    cars[carIndex] = updatedCar;
+    res.json(updatedCar);
+  }
+};
+
+export const deleteCar = (req: Request, res: Response) => {
+  const car = cars.find((c) => c.id === parseInt(req.params.id));
+  if (!car) {
+    res.status(404).send("The car with the given ID was not found.");
+    return;
+  }
+  const index = cars.indexOf(car);
+  cars.splice(index, 1);
+  res.send(car);
+};
