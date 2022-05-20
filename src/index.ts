bccdc2d0c514c3d0d21f4a5a2d2cf3d4b6cdb45b
J-aros <j.aros.cortes@gmail.com>
2022-05-20 00:43:08 -0400

    refactor: Change console.log

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
-const express = require("express");
-const app = express();
+import app from "./app";
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(app.get("port"), () => {
+  console.log(`Server is running on port ${app.get("port")}`);
 });
