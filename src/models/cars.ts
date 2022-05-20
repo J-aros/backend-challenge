2b806b3527714db8d63dae50a71a7b7f0ed7cc06
J-aros <j.aros.cortes@gmail.com>
2022-05-20 02:06:37 -0400

    feat: Add object array dummy

---
+++ src/models/cars.ts
@@ -0,0 +1,69 @@
+interface Car {
+  id: number;
+  car: string;
+  car_model: string;
+  car_model_year: number;
+}
+
+export const cars: Car[] = [
+  {
+    id: 1,
+    car: "Mitsubishi",
+    car_model: "Montero",
+    car_model_year: 2002,
+  },
+  {
+    id: 2,
+    car: "Volkswagen",
+    car_model: "Passat",
+    car_model_year: 2008,
+  },
+  {
+    id: 3,
+    car: "BMW",
+    car_model: "M6",
+    car_model_year: 2008,
+  },
+  {
+    id: 4,
+    car: "Jeep",
+    car_model: "Compass",
+    car_model_year: 2012,
+  },
+  {
+    id: 5,
+    car: "Mitsubishi",
+    car_model: "Lancer Evolution",
+    car_model_year: 2002,
+  },
+  {
+    id: 6,
+    car: "Chevrolet",
+    car_model: "Suburban",
+    car_model_year: 2009,
+  },
+  {
+    id: 7,
+    car: "Volvo",
+    car_model: "S60",
+    car_model_year: 2021,
+  },
+  {
+    id: 8,
+    car: "Mazda",
+    car_model: "Mazda 3",
+    car_model_year: 2014,
+  },
+  {
+    id: 9,
+    car: "Audi",
+    car_model: "Q7",
+    car_model_year: 2012,
+  },
+  {
+    id: 10,
+    car: "Porsche",
+    car_model: "911",
+    car_model_year: 2022,
+  },
+];
